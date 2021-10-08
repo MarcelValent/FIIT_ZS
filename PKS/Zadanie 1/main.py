f6c9ab6605a8065e89a9cb235aa109817095a94b
Marcel Valent <71660917+MarcelValent@users.noreply.github.com>
2021-10-08 14:02:55 +0200

    TOTO IDE NA ODOVZDANIE

--- PKS/Zadanie 1/main.py
+++ PKS/Zadanie 1/main.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 from binascii import hexlify, unhexlify
 
-file = rdpcap("eth-2.pcap")
+file = rdpcap("trace-27.pcap")
 encoding = "UTF-8"
 
 
@@ -36,7 +36,7 @@ def print_info(file):
         ieee = int(hexlify(ieee), 16)
         print_type(ether, ieee)
         inside_protocol(riadok, ether, ieee)
-        print_packet(riadok)
+        #print_packet(riadok)
 
 
 def print_type(ether, ieee):
@@ -51,8 +51,11 @@ def print_type(ether, ieee):
         print("IEEE 802.3 LLC")
 
 
-def extract_data(packet_data, start, end):
-    return packet_data[start:(end + 1)]
+def extract_data(packet_data, start, end, integ=False):
+    if integ:
+        return int(hexlify(packet_data[start:(end + 1)]), 16)
+    else:
+        return packet_data[start:(end + 1)]
 
 
 def print_mac(riadok):
@@ -64,16 +67,16 @@ def print_mac(riadok):
     print_packet(receive_mac)
 
 
-def load_from_file(p_filename, p_dictionary):
-    for line in open(p_filename, "r"):
+def load_from_file(file_name, dictionary):
+    for line in open(file_name, "r"):
         processed = line.split(' ', 1)
-        p_dictionary[int(processed[0], 16)] = processed[1].replace("\n", "")
+        dictionary[int(processed[0], 16)] = processed[1].replace("\n", "")
 
 
-def print_ip():
+def print_ip(sour_start, sour_end, dest_start, dest_end):
     count = 0
-    source = extract_data(riadok, 26, 29)
-    destination = extract_data(riadok, 30, 33)
+    source = extract_data(riadok, sour_start, sour_end)
+    destination = extract_data(riadok, dest_start, dest_end)
     print("Zdrojová IP adresa: ", end='')
     for k in source:
         count += 1
@@ -83,19 +86,22 @@ def print_ip():
             print(str(k) + ".", end='')
 
 
-
-    print("\nCieľová IP adresa: ", end='' )
+    print("\nCieľová IP adresa: ", end='')
     count2 = 0
     for l in destination:
         count2 += 1
         if count2 == len(destination):
-            print(str(l), end='')
+            print(str(l))
         else:
             print(str(l) + ".", end='')
-    print("\n")
 
 
-def inside_protocol(riadok,ether,ieee):
+def transport_protocol(help):
+    head = (extract_data(help, 14, 14, True) - int(extract_data(help, 14, 14, True) / 16) * 16) * 4
+    return help[14 + head:len(help)]
+
+
+def inside_protocol(riadok, ether, ieee):
     print("Typ vnoreného protokolu: ", end='')
     ether_type = {}
     llc = {}
@@ -104,40 +110,55 @@ def inside_protocol(riadok,ether,ieee):
     load_from_file("ether_types.txt", ether_type)
     load_from_file("llc_saps.txt", llc)
     load_from_file("ip_protocols.txt", ip)
-    ip_header = extract_data(riadok, 23, 23)
-    ip_header = int(hexlify(ip_header), 16)
     load_from_file("udp_ports.txt", ports)
-
+    ip_header = extract_data(riadok, 23, 23, True)
+    help3 = extract_data(riadok, 14, 14, True)
     if ip_header == 17:
         load_from_file("udp_ports.txt", ports)
     elif ip_header == 6:
         load_from_file("tcp_ports.txt", ports)
-    if ether >=1500:
-        # vypise ethertype
+    elif ip_header == 1:
+        icmp = {}
+        load_from_file("icmp_types.txt", icmp)
+
+    if ether >= 1500:
         if ether in ether_type:
             print(ether_type.get(ether))
         else:
             print("Neznámy Ethertype")
-
-    if ether == 2048:
     if ether == 2054:
+        print_ip(28, 31, 38, 41)
+        if (extract_data(riadok, 20, 21, True)) == 2:
+            print("ARP REPLY")
+        else:
+            print("ARP REQUEST")
+    if ether == 2048:
+        print_ip(26, 29, 30, 33)
+        if ip_header in ip:
+            print(ip.get(ip_header))
+
+        if ip_header == 1:
+            print("Správa: ", end="")
+            if extract_data(transport_protocol(riadok), 0, 0, True) in icmp:
+                print(icmp.get(extract_data(transport_protocol(riadok), 0, 0, True)))
+
+        help = extract_data(transport_protocol(riadok), 0, 1, True)
+        help2 = extract_data(transport_protocol(riadok), 2, 3, True)
+        if len(ports) != 0:
+            print("Zdrojový port: ", end="")
+            print(help)
+            print("Cieľový port: ", end="")
+            print(help2)
+        if help in ports:
+            print(ports.get(help))
+        elif help2 in ports:
+            print(ports.get(help2))
+        else:
+            print("Neznámy port")
+    elif help3 in llc:
+        print(llc.get(help3))
+    elif ieee == 65535:
+        print("IPX")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    print_ip()
 print_info(file)
